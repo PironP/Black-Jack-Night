add08b9d3d83dfc2d2b5228f0bcc2c1ab5696eda
PironP <piron.pierre@gmail.com>
2018-12-17 10:08:45 +0100

    initial commit

---
+++ app/src/main/java/com/pierrepiron/blackjacknight/Activity/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.pierrepiron.blackjacknight.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pierrepiron.blackjacknight.R
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
---
+++ app/src/main/java/com/pierrepiron/blackjacknight/Model/Dealer.kt
@@ -0,0 +1,4 @@
+package com.pierrepiron.blackjacknight.Model
+
+class Dealer {
+}
